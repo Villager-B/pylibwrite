0c76e2dc615757ef87a5c28b2c70067809889eb6
Villager-B <kkss07020@gmail.com>
2021-03-08 15:28:02 +0900

    [Add] first commit
    とりあえず動くライブラリとして動作している．ドキュメント周りはまだ未対応．

---
+++ README.rst
@@ -0,0 +1,3 @@
+# pylibwrite
+
+A library that creates a requirements.txt file using only the libraries listed in the python file directly below it．
---
+++ exsample/sample.py
@@ -0,0 +1,17 @@
+import os
+import sys
+import json
+import re
+import math
+import requests
+import numpy as np
+import matplotlib.pyplot as plt
+import datetime
+import time
+  import japanize_matplotlib
+ import pandas as pd
+from tomato import tomatoaaaaaaaaas
+# import sadaa
+# from sdadda
+import sqlite3
+import email
